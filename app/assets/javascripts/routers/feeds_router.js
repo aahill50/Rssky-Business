e51ee7d6926d429365ee2ffd85ff2dc1acc8fa90
App Academy Student <student@appacademy.io>
2014-10-21 11:04:39 -0400

    lists feeds on index page

---
+++ app/assets/javascripts/routers/feeds_router.js
@@ -0,0 +1,24 @@
+NewsReader.Routers.FeedsRouter = Backbone.Router.extend({
+  routes: {
+    "": "indexView"
+  },
+
+  initialize: function(options) {
+    this.$rootEl = options.$rootEl;
+  },
+
+  indexView: function() {
+    NewsReader.feeds.fetch();
+    var view = new NewsReader.Views.FeedsIndex({
+      collection: NewsReader.feeds
+    });
+
+    this._swapView(view);
+  },
+
+  _swapView: function(newView) {
+    this.currentView && this.currentView.remove();
+    this.currentView = newView;
+    this.$rootEl.html(newView.render().$el);
+  }
+});
